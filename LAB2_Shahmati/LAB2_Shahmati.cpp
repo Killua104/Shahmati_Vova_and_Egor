60dc196ac60a2ee5edcd0f7ca3df8d8a54c14711
Egor Kostin <hxh.9egor9@gmail.com>
2024-10-13 21:46:56 +0700

    Добавьте файлы проекта.

---
+++ LAB2_Shahmati/LAB2_Shahmati.cpp
@@ -0,0 +1,39 @@
+﻿struct Shahmati
+{
+	struct field
+	{
+		char kontur
+		char pole[7][7];
+	};
+
+	struct figure
+	{
+
+	};
+
+	struct color
+	{
+
+	};
+	struct motion
+	{
+
+	};
+
+	struct score
+	{
+
+	};
+
+	struct player
+	{
+		int player1;
+		int player2;
+	};
+
+	struct rules
+	{
+
+	};
+
+};
